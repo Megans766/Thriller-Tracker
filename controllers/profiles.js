7ed240a3d04d2552482fcc4583525bf32a1440e6
Megan Smith <megans766@gmail.com>
2023-01-17 00:22:03 -0500

    update toVisit view

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -3,7 +3,7 @@ import { Profile } from '../models/profile.js'
 function index(req, res) {
   Profile.find({})
   .then(profiles => {
-    res.render('profiles/index' , {
+    res.render('profiles/index', {
       title: 'Adventure Profiles',
       profiles
     })
@@ -18,7 +18,7 @@ function show(req, res) {
   Profile.findById(req.params.id)
   .then(profile => {
     const profileOwner = profile._id.equals(req.user.profile._id)
-    res.render('profiles/show' , {
+    res.render('profiles/show', {
       title: `${profile.name}'s Adventures`,
       profile,
       profileOwner
